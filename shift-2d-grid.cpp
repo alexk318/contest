a72bb4efedfd374fc2aff5497d4e61587e3dc38a
Alexander Karpenko <47079357+alexk318@users.noreply.github.com>
2026-09-20 21:11:56 +0500

    Добавлено объяснение

--- shift-2d-grid.cpp
+++ shift-2d-grid.cpp
@@ -1,5 +1,11 @@
 /*
 1260. Shift 2D Grid
+
+Для удобства введем функцию shift_right(std::vector<int>& vec), осуществляющую перестановку всех элементо вправо.
+Последний элемент становится первым.
+
+Матрица реализуется вектором строк, что удобно для перемещения первых двух столбцов влево (Просто применив (shift_right)),
+но неудобно для изменения порядка элемнтов в последнем столбце. Поэтому создается отдельный вектор, где элементы столбца расположеные горизонтально.
 */
 
 class Solution {
@@ -11,34 +17,30 @@ public:
         for (size_t i = vec.size() - 1; i > 0; --i) {
             vec[i] = vec[i - 1];
         }
-
          vec[0] = last_element;
- 
     }
 
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         std::vector<int> first_column = {};
-        first_column.reserve(grid[0].size());
+        first_column.reserve(grid[0].size());    // Для эффективности сразу займем память для вектора
 
-        for (size_t row = 0; row < grid.size(); ++row) {
-            first_column.push_back(grid[row][0]);
-        }
-
-        for (size_t _ = 0; _ < k; ++_) {
+        for (size_t _ = 0; _ < k; ++_) {    // Выполняем k итераций
 
             for (std::vector<int>& row : grid) {
                 this->shift_right(row);
             }
 
             for (size_t row = 0; row < grid.size(); ++row) {
-                first_column[row] = grid[row][0];
+                first_column.push_back(grid[row][0]);
             }
 
-            this->shift_right(first_column);
+            this->shift_right(first_column);    // Спускаем в столбце элементы вниз
 
             for (size_t i = 0; i < first_column.size(); ++i) {
-                grid[i][0] = first_column[i];
+                grid[i][0] = first_column[i];   // Заменяем первый столбец в исходной матрице
             }
+
+            first_column.clear();
         }
         
         return grid;
