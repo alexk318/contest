1dae37d4d0387a332907236c041ae895ef2c26b9
Alexander Karpenko <47079357+alexk318@users.noreply.github.com>
2026-09-20 20:55:41 +0500

    Добавлено объяснение

--- single-number-2.cpp
+++ single-number-2.cpp
@@ -1,35 +1,31 @@
 /*
 137. Single Number II (Medium)
+
+По условию задачи мы точно знаем что чисел, не равных искомому числу ровно по три штуки.
+В таком случае в отсортированном списке можно рассматривать по группы три числа. Если в данной
+группе мы найдем число, отличное от других, то это число - искомое.
 */
 
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
-
         size_t i = 2;
 
         while(i < nums.size()) {
-            // Нужно определять, идут ли три числа вместе как группа одинаковых чисел
-
-            if (nums[i] != nums[i-1] || nums[i] != nums[i-2]) {
-
-                if (nums[i] != nums[i-1]) {
-                    if (nums[i] == nums[i-2]) { return nums[i-1]; }
-                    else {return nums[i]; }
+            if (nums[i] != nums[i-1] || nums[i] != nums[i-2]) {   // Если в группе имеется одно число, не равно двум другим,
+                
+                if (nums[i] != nums[i-1]) {    // Если третье число не равно второму, 
+                    if (nums[i] == nums[i-2]) { return nums[i-1]; } // Но третье число равно первому, то искомое число - второе
+                    else { return nums[i]; }
                 }
-
-                else {
-
-                    if (nums[i] == nums[i-1]) { return nums[i-2]; }
-                    else {return nums[i]; }
-
-                }
+                else if (nums[i] == nums[i-1]) {
+                    return nums[i-2];
+                } 
+                else { return nums[i]; }
             }
-
-            i += 3;
-
+            i += 3; // Ищем в следующей тройке
         }
-        return nums[nums.size() - 1];
+        return nums[nums.size() - 1]; // Если не нашли не в одной тройке, то это означает что искомое число - последнее
     }
 };
