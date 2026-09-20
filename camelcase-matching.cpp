b2458ef872f78ae23160d265075c70acd81a337d
Alexander Karpenko <47079357+alexk318@users.noreply.github.com>
2026-09-20 21:42:22 +0500

    Добавлено объяснение

--- camelcase-matching.cpp
+++ camelcase-matching.cpp
@@ -1,12 +1,19 @@
 /*
 1023. Camelcase Matching (Medium)
+
+Идея заключается в том, чтобы разделить слова и шаблон на несколько частей, начинающихся с заглавной буквы. Все это будем хранить в векторе.
+Для этого заведем функцию split_string_by_uppercase(std::string query). Т. к. в тест-кейсах имеются случаи когда слова начинаются с маленьких букв,
+в этой функции сначала ищется индекс первой заглавной буквы first_capital_index.
+
+Далее смотрим, имеется ли в i-ой части слова все буквы из i-ой частьи шаблона. Если шаблон верный, то этих частей должно быть поровну.
+После обнаружения того, что слово не соответствует шаблону, цикл перебирающий части шаблона будет зря отрабатывать. Чтобы этого избежать используется is_correct
+
 */
 
 class Solution {
 public:
 
     vector<bool> camelMatch(vector<string>& queries, string pattern) {
-        // Разделить строку на три строки, начинающихся с заглавной буквы.
 
         std::vector<bool> result = {};
 
@@ -19,9 +26,10 @@ public:
 
         // Если шаблон подходящий, то размеры query_parts и pattern_parts одинаковые
 
-        bool is_correct = true;
+        // После обнаружения того, что слово не соответствует шаблону, цикл перебирающий части шаблона будет зря отрабатывать. Чтобы этого избежать используется is_correct
+        bool is_correct = true;     
         for (size_t q = 0; q < queries_parts.size(); ++q) {
-            if (queries_parts[q].size() != pattern_parts.size()) {
+            if (queries_parts[q].size() != pattern_parts.size()) {  // Если кол-во частей слова и шаблона разные, то оно точно не подходит
                 result.push_back(false);
             }
 
@@ -29,30 +37,27 @@ public:
                 is_correct = true;
 
                 for (size_t i = 0; i < pattern_parts.size(); ++i) {
-                    if (!is_correct) { break; }
+                    if (!is_correct) { break; }  
 
-                    // Необязательно чтобы в слове буквы шли друг за другом
-                    for (size_t letter_index = 0; letter_index < pattern_parts[i].size(); ++letter_index) {
+                    for (size_t letter_index = 0; letter_index < pattern_parts[i].size(); ++letter_index) {   // Перебираем буквы части шаблона
 
-                        if (queries_parts[q][i].find(pattern_parts[i][letter_index]) == std::string::npos) {
+                        if (queries_parts[q][i].find(pattern_parts[i][letter_index]) == std::string::npos) {   // Если не нашли букву
                             result.push_back(false);
                             is_correct = false;
                             break;
                         }
                         else {
-                            queries_parts[q][i].erase(queries_parts[q][i].find(pattern_parts[i][letter_index]), 1);
+                            // Если нашли букву, то ее стираем, т. к. может быть несколько одинаковых букв
+                            queries_parts[q][i].erase(queries_parts[q][i].find(pattern_parts[i][letter_index]), 1);   
                         }
                     }
-
-                    
                 }
 
-                if (result.size() == q) { result.push_back(true); } // Если совпало с шаблоном
-
+                // Если совпало с шаблоном, то в ответ не будет добавлен false, тем самым размер result не будет соответствовать числу рассмотренных слов -
+                // Чем и воспользуемся для определения условия, когда нужно добавлять true.
+                if (result.size() == q) { result.push_back(true); } 
             }
-
         }
-
         return result;
     }
 
@@ -73,7 +78,7 @@ public:
         for (size_t i = first_capital_index + 1; i < query.size(); ++i) {
 
             if (std::isupper(static_cast<unsigned char>(query[i]))) {
-                // Если мы наткнулись на заглавную букву, то это означает что можно подвести итог
+                // Если мы наткнулись на заглавную букву, то это означает что составление части завершено
 
                 result.push_back(current_part);
                 current_part = query[i];
